45e6ab3617ae5a267f6c506eff8a51913dbde79b
Kylie Le <kylie.tram.le@gmail.com>
2016-04-14 21:19:02 -0700

    links old-fashioned

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'products#index'
   get '/products' => 'products#index'
-
+  get'/products/:id' => 'products#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
