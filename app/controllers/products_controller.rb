cd4b78401c1c88dedab1caa90a8890cda4f1e2e6
Kylie Le <kylie.tram.le@gmail.com>
2016-04-17 13:29:54 -0700

    CRUD in REST

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,26 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find_by(id: params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @new_product = Product.new(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+    @new_product.save
+  end
+
+  def edit
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+  end
+
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+  end
 end
