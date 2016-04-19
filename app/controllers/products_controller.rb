48435ef924a6d7e93f7d2b555015c70c0adae447
Kylie Le <kylie.tram.le@gmail.com>
2016-04-18 20:27:34 -0700

    Model Methods for tax

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,8 +11,11 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @new_product = Product.new(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
-    @new_product.save
+    @product = Product.new(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+    @product.save
+
+    flash[:success] = "Product Created!"
+    redirect_to "/products/#{@product.id}"
   end
 
   def edit
@@ -22,10 +25,16 @@ class ProductsController < ApplicationController
   def update
     @product = Product.find_by(id: params[:id])
     @product.update(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+
+    flash[:success] = "Product Updated!"
+    redirect_to "/products/#{@product.id}"
   end
 
   def destroy
     @product = Product.find_by(id: params[:id])
     @product.destroy
+
+    flash[:danger] = "Product Deleted"
+    redirect_to "/products"
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ActiveRecord::Base
+  def sale_message
+    price < 2 ? "Discount" : "On Sale!"
+  end
+
+  def tax
+    (0.09 * price).to_f
+  end
+
+  def sum
+    sum = price + tax
+  end
+
 end
